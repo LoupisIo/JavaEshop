fc1997d734db43eb99576e77f369def8bc7330d4
Name <loupis.jo@gmail.com>
2022-05-06 09:46:38 +0300

    Initial Commit

---
+++ gr-upatras/src/main/java/User.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author loupis.io
+ *
+ */
+public class User {
+	public String name;
+	public String email;
+	
+	
+	
+	/**
+	 * Setters and getters
+	 * 
+	 */
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
